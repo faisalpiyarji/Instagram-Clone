1320b825dc1c223ac8212ae173b2ae11b5820213
faisalpiyarji <piyarjifaisal@gmail.com>
2021-10-12 19:56:23 +0530

    Update
    12-10-2021

--- components/Header.js
+++ components/Header.js
@@ -10,6 +10,39 @@ const Header = () => {
             source={require('../assets/header-logo.png')} 
             />
             </TouchableOpacity>
+
+            <View style={styles.iconsContainer}>
+                <TouchableOpacity>
+                    <Image
+                    source={{
+                        uri: 'https://img.icons8.com/fluency-systems-regular/60/ffffff/plus-2-math.png'
+                    }}
+                    style={styles.icon}
+                    />
+                </TouchableOpacity>
+                
+                <TouchableOpacity>
+                    <Image
+                    source={{
+                        uri: 'https://img.icons8.com/fluency-systems-regular/60/ffffff/like--v1.png'
+                    }}
+                    style={styles.icon}
+                    />
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <View style={styles.unreadBadge}>
+                        <Text style={styles.unreadBadgeText}>11</Text>
+                    </View>
+                    <Image
+                    source={{
+                        uri: 'https://img.icons8.com/fluency-systems-regular/60/ffffff/facebook-messenger.png'
+                    }}
+                    style={styles.icon}
+                    />
+                </TouchableOpacity>
+            </View>
+
         </View>
     )
 }
@@ -21,12 +54,41 @@ const styles = StyleSheet.create ({
         
     },
 
+    iconsContainer: {
+        flexDirection: 'row',
+
+    },
 
     logo: {
         width: 100,
         height: 50,
         resizeMode: 'contain',
-    }
+    },
+
+    icon: {
+        width: 30,
+        height: 30,
+        marginLeft: 10,
+        resizeMode: 'contain',
+    },
+
+    unreadBadge: {
+        backgroundColor: '#FF3250',
+        position: 'absolute',
+        left: 20,
+        bottom: 18,
+        width: 25,
+        height: 18,
+        borderRadius: 25,
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 100,
+    },
+
+    unreadBadgeText: {
+        color: 'white',
+        fontWeight: '600',
+    },
 })
 
 export default Header
---
+++ data/users.js
@@ -0,0 +1,15 @@
+export const USER = [
+    {
+        user: 'faisalpiyarji',
+        image:
+        'https://scontent.fbom19-1.fna.fbcdn.net/v/t1.6435-9/75412130_114468130003824_1071370959102935040_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=nY-WwGz5WhkAX-BVoFy&_nc_ht=scontent.fbom19-1.fna&oh=7cc024ea6dba840c65f29aa4f684f1cf&oe=618CF64B',        
+    },
+
+    {
+        user: 'riyazuddinkhan',
+        image:
+        'https://scontent.fbom19-1.fna.fbcdn.net/v/t1.18169-9/19990229_892898004191198_4373967826005957767_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=MN3YladNeiYAX_LpSfY&tn=6wNcI6TImPSCcy9Y&_nc_ht=scontent.fbom19-1.fna&oh=4059b8c35969041972d60f4e2fa99503&oe=618C5440',        
+    },
+
+
+]
