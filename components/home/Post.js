c20f174f858ab93a14770bd10a5c448b60451867
faisalpiyarji <piyarjifaisal@gmail.com>
2021-10-13 15:28:34 +0530

    add post component
    13-10-2021 3:28

---
+++ components/home/Post.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { View, Text, Image } from 'react-native'
+import { Divider } from 'react-native-elements'
+
+const Post = ({post}) => {
+    return (
+        <View style={{marginBottom: 30}}>
+            <Divider width={1} orientation='vertical' />
+            <PostHeader post={post} />
+        </View>
+    )
+}
+
+const PostHeader = ({post}) => (
+    <View>
+        <View>
+            <Image />
+            <Text></Text>
+        </View>
+    </View>
+)
+
+export default Post
---
+++ data/posts.js
@@ -0,0 +1,34 @@
+import { USER } from './users'
+
+export const POSTS = [
+    {
+        imageUrl: 'https://instagram.fbom26-1.fna.fbcdn.net/v/t51.2885-15/e35/c315.0.810.810a/s320x320/243198463_322916886257296_8952061520707742732_n.jpg?_nc_ht=instagram.fbom26-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=U3u51SPEXAAAX9A-Pol&edm=ABfd0MgBAAAA&ccb=7-4&oh=5080a40e1807ca57f0f9db55078879a9&oe=616DC6D6&_nc_sid=7bff83',
+        user: USER[0].user,
+        likes: 9862,
+        caption: 'Bungalow',
+        profile_picture: USER[0].image,
+        comments: [
+            {
+                user: 'faisalpiyarji',
+                comment: 'Wow'
+            },
+        ],
+
+    },
+
+    {
+        imageUrl: 'https://instagram.fbom26-1.fna.fbcdn.net/v/t51.2885-15/e35/c315.0.810.810a/s320x320/243198463_322916886257296_8952061520707742732_n.jpg?_nc_ht=instagram.fbom26-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=U3u51SPEXAAAX9A-Pol&edm=ABfd0MgBAAAA&ccb=7-4&oh=5080a40e1807ca57f0f9db55078879a9&oe=616DC6D6&_nc_sid=7bff83',
+        user: USER[0].user,
+        likes: 9862,
+        caption: 'Bungalow',
+        profile_picture: USER[0].image,
+        comments: [
+            {
+                user: 'faisalpiyarji',
+                comment: 'Wow'
+            },
+        ],
+
+    },
+]   
+
